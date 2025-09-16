4bb0ffc505077534df814bf3e8187a2a0e385884
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-09-16 16:50:50 +0000

    aa

--- epidemietrackr/urls.py
+++ epidemietrackr/urls.py
@@ -9,7 +9,7 @@ from epidemie.views import LandingPageView, EpidemieDetailView, PatientCreateVie
     InformationCreateView, import_data, import_synthese_view
 
 urlpatterns = [
-                  path("healthz", lambda r: HttpResponse("ok")),
+                  path("health", lambda r: HttpResponse("ok")),
                   path('admin/', admin.site.urls),
                   path('api-auth/', include('rest_framework.urls')),
                   # path('api/', include(router.urls)),
